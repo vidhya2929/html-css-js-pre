5dd01eefa0bb4c11f4d046beaa3aaf6cab8299c4
Vidhya <vidhyamrobert.29@gmail.com>
2025-08-09 23:47:23 +0530

    p5.js

--- js basic/pa5.js
+++ js basic/pa5.js
@@ -1,10 +1,19 @@
 
-let originalArray = [12,23,43,54,65];
-let reversedArray = [];
+// let originalArray = [12,23,43,54,65];
+// let reversedArray = [];
 
-for(let i = originalArray.length-1;i>=0;i--){
-  reversedArray.push(OriginalArray[i]);
+// for(let i = originalArray.length-1;i>=0;i--){
+//   reversedArray.push(originalArray[i]);
+// }
+// console.log("Reversed Array: ",reversedArray);
+
+
+
+
+let array = [12,3,23,54,767,67];
+let reverse = [];
+
+for(let i = array.length-1;i>=0;i--){
+reverse = push(array[i]);
 }
-console.log("Reversed Array: ",reversedArray);
-
-// 
+console.log("Reversed Array: ",reverse);
